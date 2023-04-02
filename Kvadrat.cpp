8816b2a0454e229dc757b7bde69ea6a430bfda47
SAYL <sakovetclesha@gmail.com>
2023-04-02 15:22:04 +0300

    Ended program kvadrat

---
+++ Kvadrat.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int array[5][5];
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            array[i][j] = 0;
+            array[i][4 - i] = 1;
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << array[i][j];            
+        }
+        cout << "\n";
+    }
+}
+
+
